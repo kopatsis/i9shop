654aee37adb42d05778a035379dbdc97d152c7b4
jwork <jkopatsis@gmail.com>
2024-04-12 13:04:51 -0700

    template

--- platform/router.go
+++ platform/router.go
@@ -14,10 +14,21 @@ func New() *gin.Engine {
 	router.Use(middleware.CORSMiddleware())
 
 	router.LoadHTMLGlob("templates/*")
-	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
-	router.GET("/index", func(c *gin.Context) {
+	router.GET("/public", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title": "Main website",
+			"Title":   "Public Page",
+			"Header":  "Welcome to Public Page",
+			"Message": "Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah",
+			"Year":    2024,
+		})
+	})
+
+	router.GET("/unprotected", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"Title":   "Private Page",
+			"Header":  "Welcome to Private Page",
+			"Message": "Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah",
+			"Year":    2024,
 		})
 	})
 
